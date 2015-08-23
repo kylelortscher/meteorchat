0eb36e159d1d28421f5b3b3562d79ecbb0d1ee5e
Kyle Lortscher <kylelortscher@gmail.com>
2015-08-22 23:44:08 -0700

    added logic to return usernames when a chat is made

--- filive-chat.js
+++ filive-chat.js
@@ -16,13 +16,21 @@ if (Meteor.isClient) {
 
         Messages.insert({
           message: value,
-          timestamp: new Date()
-          // user: Meteor.userId()
+          timestamp: new Date(),
+          user: Meteor.userId()
         });
       }
     }
   });
 
+  //Logic To Return Usernames Of User That Post A Chat Message
+  Template.message.helpers({
+    user: function(){
+      return Meteor.users.findOne ({_id: this.user});
+    }
+  });
+
+  //Making User and Email Singup
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_AND_EMAIL"
   });
